95d813f4c225d3ac728ce6cabf800f19fe02c273
0x2Elang <224955561+0x2Elang@users.noreply.github.com>
2026-04-25 13:13:52 +0200

    First commit.

---
+++ .install-bitwarden.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+# exit immediately if password-manager-binary is already in $PATH
+type bw >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Linux)
+    . /etc/os-release
+    case "$ID" in
+        endeavouros|arch)
+            sudo pacman -S --noconfirm bitwarden-cli
+            ;;
+    *)
+        echo "unsupported distro: $ID" >&2
+        exit 1
+        ;;
+    esac
+   ;;
+*)
+    echo "unsupported OS: $(uname -s)" >&2
+    exit 1
+    ;;
+esac
